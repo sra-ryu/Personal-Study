da1de0fe8f9a92dad054cd05edd0dd876ef4c42f
comdobby <tofk1107@gmail.com>
2025-11-14 17:31:59 +0100

    practice filter

--- Haskell/functional_concept/higher_order_functions.hs
+++ Haskell/functional_concept/higher_order_functions.hs
@@ -15,3 +15,16 @@ squareAll = map (^ 2)
 -- calculate a length of strings
 lengths :: [String] -> [Int]
 lengths = map length
+
+--- higher order functions - filter ---
+-- filter elements that greater than 10
+greaterThan10 :: [Int] -> [Int]
+greaterThan10 = filter (> 10)
+
+-- filter even numbers
+onlyEvens :: [Int] -> [Int]
+onlyEvens = filter ((== 0) . (`mod` 2))
+
+-- filter string that length == 2
+length' :: [String] -> [String]
+length' = filter ((== 2) . length)

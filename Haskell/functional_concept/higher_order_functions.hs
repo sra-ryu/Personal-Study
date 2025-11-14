fd5d068152148e80115202a78cabaa948a58e3e3
comdobby <tofk1107@gmail.com>
2025-11-14 16:33:34 +0100

    practice map

--- Haskell/functional_concept/higher_order_functions.hs
+++ Haskell/functional_concept/higher_order_functions.hs
@@ -1,2 +1,17 @@
+import Data.Char (toUpper)
+
 applyThrice :: (a -> a) -> a -> a
 applyThrice f x = f (f (f x))
+
+--- higher order functions - map ---
+-- add 10 to elements
+add10 :: [Int] -> [Int]
+add10 = map (10 +)
+
+-- square all elements of a list
+squareAll :: [Int] -> [Int]
+squareAll = map (^ 2)
+
+-- calculate a length of strings
+lengths :: [String] -> [Int]
+lengths = map length

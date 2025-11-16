1b3c06f1518b6ad8d573545f33a7c4bec55b3e31
comdobby <tofk1107@gmail.com>
2025-11-16 17:11:29 +0100

    practice fold

--- Haskell/functional_concept/higher_order_functions.hs
+++ Haskell/functional_concept/higher_order_functions.hs
@@ -3,6 +3,7 @@ import Data.Char (toUpper)
 applyThrice :: (a -> a) -> a -> a
 applyThrice f x = f (f (f x))
 
+
 --- higher order functions - map ---
 -- add 10 to elements
 add10 :: [Int] -> [Int]
@@ -16,6 +17,7 @@ squareAll = map (^ 2)
 lengths :: [String] -> [Int]
 lengths = map length
 
+
 --- higher order functions - filter ---
 -- filter elements that greater than 10
 greaterThan10 :: [Int] -> [Int]
@@ -28,6 +30,7 @@ onlyEvens = filter ((== 0) . (`mod` 2))
 -- filter string that length == 2
 length' :: [String] -> [String]
 length' = filter ((== 2) . length)
+
 
 --- higher order functions - mix ---
 -- select elements that even and divide by 2
@@ -45,3 +48,20 @@ halveEvens' xs = map (`div` 2) (filter even xs)
 longToUpper :: [String] -> [String]
 longToUpper = map (map toUpper) . filter ((> 3) . length)
 -- string = [Char], list of string = [[Char], [Char], ...]
+
+
+--- higher order functions - fold ---
+-- product all elements of a list
+productList :: [Int] -> Int
+productList = foldr (*) 1
+
+-- concatenate all elements of a list
+concatWords :: [String] -> String
+concatWords = foldl go ""
+    where
+        go "" s = s
+        go s1 s2 = s1 ++ " " ++ s2
+
+-- find a max element(int)
+maxList :: [Int] -> Int
+maxList (x:xs) = foldr max x xs

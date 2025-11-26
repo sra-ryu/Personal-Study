e23321e3e4c18de2bf4acb8b4104342299757589
comdobby <tofk1107@gmail.com>
2025-11-26 19:03:56 +0100

    add get api for read user information

--- Java/library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ Java/library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -2,6 +2,8 @@ package com.group.libraryapp.controller.user;
 
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +28,21 @@ public class UserController {
     @PostMapping("/user")
     public void saveUser(@RequestBody UserCreateRequest request) {
         users.add(new User(request.getName(), request.getAge()));
+    }
+
+    /* todo: get user information
+    *
+    * HTTP Method: GET
+    * HTTP Path:   /user
+    * Query:       no query here
+    * return:      id, name, age(JSON)
+     */
+    @GetMapping("/user")
+    public List<UserResponse> getUsers() {
+        List<UserResponse> responses = new ArrayList<>();
+        for (int i = 0; i < users.size(); i++) {
+            responses.add(new UserResponse(i + 1, users.get(i)));
+        }
+        return responses;
     }
 }

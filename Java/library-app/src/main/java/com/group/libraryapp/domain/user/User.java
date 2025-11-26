421dcadf233ea797ae46fb43c926e7eedc422522
comdobby <tofk1107@gmail.com>
2025-11-26 18:42:42 +0100

    make User class for manage list of user

---
+++ Java/library-app/src/main/java/com/group/libraryapp/domain/user/User.java
@@ -0,0 +1,15 @@
+package com.group.libraryapp.domain.user;
+
+public class User {
+
+    private String name;
+    private Integer age;
+
+    public User(String name, Integer age) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException(String.format("invalid input: name(%s)", name));
+        }
+        this.name = name;
+        this.age = age;
+    }
+}

c6fd38fb018f1997f2a4edec66be63eee472bea0
comdobby <tofk1107@gmail.com>
2025-12-03 01:00:49 +0100

    feat: add updateUser for change name of user

--- Java/library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ Java/library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -2,13 +2,11 @@ package com.group.libraryapp.controller.user;
 
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -31,7 +29,8 @@ public class UserController {
     * HTTP Method: POST
     * HTTP Path:   /user
     * HTTP Body:   name, age(JSON)
-    * return nothing(void)
+    * return:      X (HTTP status)
+    *
     * */
     @PostMapping("/user")
     public void saveUser(@RequestBody UserCreateRequest request) {
@@ -39,13 +38,15 @@ public class UserController {
         jdbcTemplate.update(sql, request.getName(), request.getAge());
     }
 
-    /* todo: get user information
+    /*
+    * todo: get user information
     *
     * HTTP Method: GET
     * HTTP Path:   /user
     * Query:       no query here
     * return:      id, name, age(JSON)
-     */
+    *
+    * */
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
         String sql = "SELECT * FROM user";
@@ -57,4 +58,21 @@ public class UserController {
             return new UserResponse(id, name, age);
         });
     }
+
+    /*
+    * todo: update user name
+    *
+    * HTTP Method: PUT
+    * HTTP Path:   /user
+    * HTTP Body:   JSON(id, name)
+    * return:      X (HTTP status)
+    *
+    * */
+    @PutMapping("/user")
+    public void updateUser(@RequestBody UserUpdateRequest request) {
+        String sql = "UPDATE user SET name = ? WHERE id = ?";
+        jdbcTemplate.update(sql, request.getName(), request.getId());
+    }
+
+
 }

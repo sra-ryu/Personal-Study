5b4ac290052edf582efa9d60963e06f1470c982c
comdobby <tofk1107@gmail.com>
2025-12-04 20:23:12 +0100

    java class for learn using java code in Kotlin

---
+++ Kotlin/Person.java
@@ -0,0 +1,16 @@
+import org.jetbrains.annotations.Nullable;
+
+public class Person {
+
+    private final String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    @Nullable
+    public String getName() {
+        return name;
+    }
+
+}

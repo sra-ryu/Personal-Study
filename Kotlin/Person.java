f64a6d9dbb4e0901e1e919e0f3d7721ee0333b48
comdobby <tofk1107@gmail.com>
2025-12-06 15:34:17 +0100

    feat: add age field

--- Kotlin/Person.java
+++ Kotlin/Person.java
@@ -3,14 +3,20 @@ import org.jetbrains.annotations.Nullable;
 public class Person {
 
     private final String name;
+    private final int age;
 
-    public Person(String name) {
+    public Person(String name, int age) {
         this.name = name;
+        this.age = age;
     }
 
     @Nullable
     public String getName() {
         return name;
+    }
+
+    public int getAge() {
+        return age;
     }
 
 }

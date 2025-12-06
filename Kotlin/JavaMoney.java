89cd6d8adcf54cec9951cb82e25d5470db155191
comdobby <tofk1107@gmail.com>
2025-12-06 16:08:32 +0100

    feat: add java class for practice compare operator in Kotlin

---
+++ Kotlin/JavaMoney.java
@@ -0,0 +1,15 @@
+import org.jetbrains.annotations.NotNull;
+
+public class JavaMoney implements Comparable<JavaMoney> {
+
+    private final long amount;
+
+    public JavaMoney(long amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public int compareTo(@NotNull JavaMoney obj) {
+        return Long.compare(this.amount, obj.amount);
+    }
+}
